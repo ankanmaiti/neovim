f77767ffd82d7646bedad7b902c50e3e0284daf1
ankanmaiti <ankanmaiti2@gmail.com>
2024-01-25 19:46:38 +0530

    update colorscheme

--- config/keymaps.lua
+++ config/keymaps.lua
@@ -27,3 +27,8 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
 vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
 vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
 vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
+
+
+-- Lazy Package Manager
+opts.desc = "Lazy"
+vim.keymap.set('n', "<leader>l", ":Lazy<cr>", opts)
--- plugins/colorschemes.lua
+++ plugins/colorschemes.lua
@@ -3,5 +3,8 @@ return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
+  config = function ()
+    vim.cmd([[colorscheme tokyonight-moon]])
+  end
 },
 }
