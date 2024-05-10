6858dcdf1ad8fb2fa9a660021f05efc5fd4e75a2
ankanmaiti <ankanmaiti2@gmail.com>
2024-05-10 07:33:59 +0530

    remove: vim-fugitive for lazygit

--- plugins/git.lua
+++ plugins/git.lua
@@ -19,17 +19,4 @@ return {
 			vim.keymap.set("n", "<leader>gl", gitsigns.toggle_current_line_blame, opts)
 		end,
 	},
-  -- run git command in neovim
-  {
-    'tpope/vim-fugitive',
-
-    config = function ()
-      
-			-- setup keymaps
-			local opts = { noremap = true, silent = true }
-
-			opts.desc = "Blame History"
-			vim.keymap.set("n", "<leader>gb", ':Git blame<CR>', opts)
-    end
-  }
 }
