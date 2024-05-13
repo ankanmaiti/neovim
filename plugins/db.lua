40653bcd3e2aa8ba5e2e1c11602a345fad147bf7
ankanmaiti <ankanmaiti2@gmail.com>
2024-05-13 14:25:51 +0530

    feature: database
    - using dadbod nivm

---
+++ plugins/db.lua
@@ -0,0 +1,18 @@
+return {
+	"kristijanhusak/vim-dadbod-ui",
+  event="VeryLazy",
+	dependencies = {
+		"tpope/vim-dadbod",
+		"kristijanhusak/vim-dadbod-completion",
+	},
+	cmd = {
+		"DBUI",
+		"DBUIToggle",
+		"DBUIAddConnection",
+		"DBUIFindBuffer",
+	},
+	init = function()
+		-- Your DBUI configuration
+		vim.g.db_ui_use_nerd_fonts = 1
+	end,
+}
