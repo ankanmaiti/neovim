2ebbf42451917d32fe6efadb4ec1baa79417e2c6
ankanmaiti <ankanmaiti2@gmail.com>
2024-08-04 20:44:43 +0530

    add: formatterh and diagonistics for php, laravel (pint)

--- plugins/none-ls.lua
+++ plugins/none-ls.lua
@@ -1,23 +1,29 @@
 return {
-  "nvimtools/none-ls.nvim",
-  event="VeryLazy",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettierd,
-        require("none-ls.diagnostics.eslint_d"), -- requires none-ls-extras.nvim
-      },
-    })
-    -- set keymap
-    local keyopts = { noremap = true, silent = true }
+		null_ls.setup({
+			sources = {
+				-- formatter
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettierd,
+				null_ls.builtins.formatting.blade_formatter,
+				null_ls.builtins.formatting.pint,
 
-    keyopts.desc = "Format Code"
-    vim.keymap.set({ "n", "v" }, "<leader>cf", vim.lsp.buf.format, keyopts)
-  end,
+				-- diagnostics
+				require("none-ls.diagnostics.eslint_d"), -- requires none-ls-extras.nvim
+				null_ls.builtins.diagnostics.phpstan,
+			},
+		})
+		-- set keymap
+		local keyopts = { noremap = true, silent = true }
+
+		keyopts.desc = "Format Code"
+		vim.keymap.set({ "n", "v" }, "<leader>cf", vim.lsp.buf.format, keyopts)
+	end,
 }
