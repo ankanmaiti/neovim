5cf3a87f613d1aac2246302b254340cd3f36fc56
ankanmaiti <ankanmaiti2@gmail.com>
2024-08-05 09:43:47 +0530

    add: frecency, noice

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -5,9 +5,12 @@ return {
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-tree/nvim-web-devicons",
+			"AckslD/nvim-neoclip.lua",
+			"folke/noice.nvim",
 			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 			{ "nvim-telescope/telescope-live-grep-args.nvim", version = "^1.0.0" },
 			{ "nvim-telescope/telescope-ui-select.nvim" },
+			{ "nvim-telescope/telescope-frecency.nvim" },
 		},
 
 		config = function()
@@ -16,12 +19,32 @@ return {
 
 			telescope.setup({
 				defaults = {
-					file_ignore_patterns = { "node_modules", ".git" },
+					file_ignore_patterns = { "node_modules", ".git", "vendor" },
 					mappings = {
 						i = {
 							["<C-k>"] = actions.move_selection_previous, -- move to prev result
 							["<C-j>"] = actions.move_selection_next, -- move to next result
-							["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+							-- freeze the current list and start a fuzzy search in the frozen list
+							["<C-space>"] = actions.to_fuzzy_refine,
+							-- ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+						},
+					},
+				},
+				extensions = {
+					frecency = {
+						show_filter_column = false,
+						show_scores = true,
+						show_unindexed = true,
+						auto_validate = true,
+						db_validate_threshold = 10,
+						db_safe_mode = true,
+
+						ignore_patterns = {
+							"*/.git",
+							"*/.git/*",
+							"*/.DS_Store",
+							"*/vendor/*",
+							"*/node_modules/*",
 						},
 					},
 				},
@@ -31,6 +54,8 @@ return {
 			telescope.load_extension("fzf")
 			telescope.load_extension("live_grep_args")
 			telescope.load_extension("ui-select")
+			telescope.load_extension("frecency")
+			telescope.load_extension("noice")
 
 			-- set keymaps
 			local keymap = vim.keymap -- for concisness
@@ -38,7 +63,7 @@ return {
 
 			keyopts.desc = "Find Files (cwd)"
 			keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", keyopts)
-			keymap.set("n", "<C-p>", "<cmd>Telescope find_files<cr>", keyopts)
+			keymap.set("n", "<C-p>", ":Telescope frecency workspace=CWD<cr>", keyopts)
 
 			keyopts.desc = "Find Recent Files"
 			keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", keyopts)
