89224b85e6b16db66fceb3d777a965ea46da49d4
Ankan Maiti <ankanmaiti2@gmail.com>
2024-08-14 14:53:07 +0530

    disable: shortcut keymap

--- plugins/git.lua
+++ plugins/git.lua
@@ -10,13 +10,13 @@ return {
 			gitsigns.setup()
 
 			-- setup keymaps
-			local opts = { noremap = true, silent = true }
-
-			opts.desc = "preview hunk"
-			vim.keymap.set("n", "<leader>gp", gitsigns.preview_hunk, opts)
-
-			opts.desc = "line blame"
-			vim.keymap.set("n", "<leader>gl", gitsigns.toggle_current_line_blame, opts)
+			-- local opts = { noremap = true, silent = true }
+			--
+			-- opts.desc = "preview hunk"
+			-- vim.keymap.set("n", "<leader>gp", gitsigns.preview_hunk, opts)
+			--
+			-- opts.desc = "line blame"
+			-- vim.keymap.set("n", "<leader>gl", gitsigns.toggle_current_line_blame, opts)
 		end,
 	},
 }
